ae546e74d9607ad59933bdd742a42b144edcfb16
Mudasir Nizamani <mudasiralinizamani@gmail.com>
2022-01-22 04:25:47 -0800

    added MaxLength in CreateFacultyDto

--- Dtos/CreateFacultyDto.cs
+++ Dtos/CreateFacultyDto.cs
@@ -3,6 +3,7 @@ namespace university_management_api.Dtos;
 public class CreateFacultyDto
 {
   [Required]
+  [MaxLength(20)]
   public string Name { get; set; } = string.Empty;
   [Required]
   public string DeanId { get; set; } = string.Empty;
